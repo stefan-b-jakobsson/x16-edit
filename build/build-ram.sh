7c3535cd70f39afcde29339764f510106fd31b47
Stefan Jakobsson <stefan.b.jakobsson@gmail.com>
2020-12-26 16:36:38 +0200

    Updated build scripts using variable segment

--- build/build-ram.sh
+++ build/build-ram.sh
@@ -2,4 +2,4 @@
 BUILD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SRC_PATH="$BUILD_PATH"/..
 
-cl65 -v --asm-args -Dtarget_mem=1 -o "$BUILD_PATH/X16EDIT.PRG" -u __EXEHDR__ -t cx16 -C cx16-asm.cfg $SRC_PATH/main.asm
+cl65 -v --asm-args -Dtarget_mem=1 -o "$BUILD_PATH/X16EDIT.PRG" -u __EXEHDR__ -t cx16 -C cx16-asm.cfg --mapfile x16edit-ram.map -vm $SRC_PATH/main.asm
